1d90824ddd6460269d58b0d8ed5bdae0392b81af
Shailesh <shaileshdpatil1@gmail.com>
2024-11-20 09:53:53 +0530

    Cors added cred

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = require('./Routes/routes');
 const cors = require('cors')
 const connectDb = require("./utils/db");
+const {GetAccessTokenProfile} = require("./Routes/routes");
 const app = express()
 
 const corsOptions = {
@@ -11,13 +12,34 @@ const corsOptions = {
 	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 	optionsSuccessStatus: 204
 };
+mongoose.set('strictQuery', true);
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Handle preflight requests
 app.use(express.json());
 
-//cors
-mongoose.set('strictQuery', true);
-app.use('/api',router);
+app.post("/api/companyprofile", async (req, res) => {
+	const access_token = await GetAccessTokenProfile();
+
+	console.log(req.body, "body", access_token);
+	axios
+		.post(
+			"https://bpa-uat-australiaeast.chainthat.net/connector-api/route/companyInformation/v1/companyProfile",
+			{
+				companyIdentifierType: "ABN",
+				identifierValue: req.body.abn,
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${access_token}`, // Include the Bearer token in the Authorization header
+				},
+			}
+		)
+		.then((response) => {
+			res.send({ success: true, companyprofile: response.data });
+		})
+		.catch((err) => {
+			res.send({ success: false, message: err });
+		});
+});
 
 const PORT = 3009;
 connectDb().then(()=>{
